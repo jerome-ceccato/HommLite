be915427c0fb7f2b2880754ffcb45c38923a518f
IATGOF <jerome.ceccato@gmail.com>
2021-01-09 19:16:14 +0100

    polishing the logs

--- hommlite/combat/ui/CombatLogs.gd
+++ hommlite/combat/ui/CombatLogs.gd
@@ -1,27 +1,42 @@
 extends RichTextLabel
 
+export(Color) var death_color: Color
+
 func _on_Battle_new_combat_log(entry: BattleLogger.Entry):
-	if get_total_character_count() > 0:
-		text += "\n"
 	append_bbcode(_entry_representation(entry))
 
+
 func _entry_representation(entry: BattleLogger.Entry) -> String:
-	var content = "The %s %s %s damage." % [
+	return "%s%s%s" % [
+		"\n" if get_total_character_count() > 0 else "",
+		_damage_text(entry),
+		_death_text(entry)
+	]
+
+func _damage_text(entry: BattleLogger.Entry) -> String:
+	return "The %s %s %s damage." % [
 		_puralized_name(entry.source.stack.unit, entry.source.amount),
 		_pluralize(entry.source.amount, "does", "do"),
-		entry.damage
+		entry.damage,
 	]
-	
+
+func _death_text(entry: BattleLogger.Entry) -> String:
 	if entry.death > 0:
-		content += " %s %s %s." % [
-			entry.death,
+		return " %s %s %s." % [
+			_colored(entry.death, death_color),
 			_puralized_name(entry.target.stack.unit, entry.death),
 			_pluralize(entry.death, "dies", "die"),
 		]
-	return content
+	else:
+		return ""
+
 
 func _puralized_name(unit: UnitData, amount: int):
 	return _pluralize(amount, unit.display_name, unit.display_name_plural)
 
 func _pluralize(count: int, singular: String, plural: String) -> String:
 	return singular if count <= 1 else plural
+
+func _colored(content, color: Color) -> String:
+	var color_code = "#" + color.to_html(false)
+	return "[color=%s]%s[/color]" % [color_code, content]
