50936375fcca10aaa820384c913d23def0a04de7
IATGOF <jerome.ceccato@gmail.com>
2021-02-02 17:28:12 +0100

    Fixed crash in CursorState

--- hommlite/src/combat/ui/actions/CursorActionResolver.gd
+++ hommlite/src/combat/ui/actions/CursorActionResolver.gd
@@ -4,53 +4,51 @@ extends Node
 var battle: Battle
 var hexgrid: HexGrid
 
-var _state: CursorState
-
 
 func setup(_battle: Battle, _hexgrid: HexGrid):
 	battle = _battle
 	hexgrid = _hexgrid
-	_state = CursorState.new()
 
 
 func get_state(mouse_pos: Vector2) -> CursorState:
-	_state.mouse_pos = mouse_pos
+	var state = CursorState.new()
+	state.mouse_pos = mouse_pos
 	var coords = hexgrid.get_cell_coords_at_point(mouse_pos)
-	_state.hovered_cell_coords = coords
-	_state.hover_hex_cells = []
+	state.hovered_cell_coords = coords
 	
 	if coords != null:
-		_state.active_stack = battle.get_active_stack()
-		_state.target_stack = battle.get_stack_at_coords(coords)
+		state.hover_hex_cells = [hexgrid.get_cell_at_coords(coords)]
+		state.active_stack = battle.get_active_stack()
+		state.target_stack = battle.get_stack_at_coords(coords)
 		
-		if _state.target_stack != null and !_target_is_large_self(_state.active_stack, _state.target_stack, coords):
-			var is_enemy = _state.target_stack.side != _state.active_stack.side
+		if state.target_stack != null and !_target_is_large_self(state.active_stack, state.target_stack, coords):
+			var is_enemy = state.target_stack.side != state.active_stack.side
 			
-			if is_enemy and battle.can_attack_ranged(_state.active_stack):
-				_state.action = CursorState.Action.RANGED_REACHABLE_STACK
+			if is_enemy and battle.can_attack_ranged(state.active_stack):
+				state.action = CursorState.Action.RANGED_REACHABLE_STACK
 			else:
-				var can_attack = battle.can_attack(_state.active_stack, _state.target_stack)
+				var can_attack = battle.can_attack(state.active_stack, state.target_stack)
 				if is_enemy and can_attack:
-					_state.hover_hex_cells = _closest_reachable_cells(_state.target_stack, coords, mouse_pos)
-					_state.action = CursorState.Action.REACHABLE_STACK
+					state.hover_hex_cells = _closest_reachable_cells(state.target_stack, coords, mouse_pos)
+					state.action = CursorState.Action.REACHABLE_STACK
 				else:
-					_state.action = CursorState.Action.UNREACHABLE_STACK
+					state.action = CursorState.Action.UNREACHABLE_STACK
 		else:
-			if battle.can_reach(_state.active_stack, coords):
-				_state.hover_hex_cells = _hover_cells(_state.active_stack, coords, battle.reachable_coords(_state.active_stack))
-				if !_move_is_identity(_state.active_stack, _state.hover_hex_cells):
-					_state.action = CursorState.Action.REACHABLE_CELL
+			if battle.can_reach(state.active_stack, coords):
+				state.hover_hex_cells = _hover_cells(state.active_stack, coords, battle.reachable_coords(state.active_stack))
+				if !_move_is_identity(state.active_stack, state.hover_hex_cells):
+					state.action = CursorState.Action.REACHABLE_CELL
 				else:
-					_state.action = CursorState.Action.UNREACHABLE_CELL
+					state.action = CursorState.Action.UNREACHABLE_CELL
 			else:
-				_state.action = CursorState.Action.UNREACHABLE_CELL
+				state.action = CursorState.Action.UNREACHABLE_CELL
 	else:
-		_state.active_stack = null
-		_state.target_stack = null
-		_state.hover_hex_cells = []
-		_state.action = CursorState.Action.NONE
+		state.active_stack = null
+		state.target_stack = null
+		state.hover_hex_cells = []
+		state.action = CursorState.Action.NONE
 	
-	return _state
+	return state
 
 
 func _move_is_identity(stack: BattleStack, hover_cells: Array) -> bool:
@@ -97,6 +95,7 @@ func _closest_reachable_cells(_target: BattleStack, target_coords: BattleCoords,
 	var neighbors = battle.get_grid().valid_neighbors(target_coords)
 	
 	# TODO: this is wasteful
+	_sort_mouse_pos = mouse
 	neighbors.sort_custom(self, "_sort_closest")
 	for coords in neighbors:
 		if _coords_in_stack(coords, active_stack) or battle.can_reach(active_stack, coords):
@@ -126,10 +125,11 @@ func _coords_in_stack(coords: BattleCoords, stack: BattleStack) -> bool:
 	return false
 
 
+var _sort_mouse_pos: Vector2
 func _sort_closest(a: BattleCoords, b: BattleCoords) -> bool:
 	var a_pos = hexgrid.get_cell_at_coords(a).center
 	var b_pos = hexgrid.get_cell_at_coords(b).center
-	var a_dist = abs(a_pos.distance_to(_state.mouse_pos))
-	var b_dist = abs(b_pos.distance_to(_state.mouse_pos))
+	var a_dist = abs(a_pos.distance_to(_sort_mouse_pos))
+	var b_dist = abs(b_pos.distance_to(_sort_mouse_pos))
 	
 	return a_dist < b_dist
