adec93ab14342782220c32b4cc47e21e62ca0115
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-04-15 18:27:23 +0200

    Hide selector when hex is empty

--- hommlite/src/adventure/Adventure.gd
+++ hommlite/src/adventure/Adventure.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
+onready var tilemap: TileMap = $TileMap
 onready var hexmap: HexMap = $TileMap/HexMap
 
 func _ready():
-	var cell_size = $TileMap.cell_size
+	var cell_size = tilemap.cell_size
 	hexmap.set_flat(true)
 	hexmap.set_origin(Vector2(-cell_size.x / 2 - 0.5, -0.5))
 	hexmap.set_size(Vector2(
 		cell_size.x / 1.5, 
 		cell_size.y / (2 * sin(PI/3))
 	))
+	
+	for cell in tilemap.get_used_cells():
+		var pos = tilemap.map_to_world(cell)
+		var hex_pos = hexmap.pixel_to_hex(pos)
+		hexmap.add_hex(hex_pos, {})
 	
 	$Selector.hexmap = hexmap
--- hommlite/src/adventure/Selector.gd
+++ hommlite/src/adventure/Selector.gd
@@ -4,4 +4,5 @@ var hexmap: HexMap
 
 func _process(delta):
 	var hex = hexmap.pixel_to_hex(get_global_mouse_position())
+	visible = hexmap.get_hex(hex) != null
 	position = hexmap.hex_to_pixel(hex)
