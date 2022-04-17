8399cdf001145f7d3a55fb7b9624739d00df7f80
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-04-17 23:14:44 +0200

    map generation with borders

--- hommlite/src/adventure/map/AdventureMap.gd
+++ hommlite/src/adventure/map/AdventureMap.gd
@@ -30,62 +30,14 @@ func _setup_hexmap():
 	))
 
 
-func _init_from_tilemap():
-	for cell in baseTilemap.get_used_cells():
-		var hex_pos = hexmap.oddq_to_axial(cell)
-		hexmap.add_hex(hex_pos, {})
-
-
 func _procedural_init():
 	baseTilemap.clear()
 	
-	var noise = _procedural_setup()
-	var radius = 16
-	for q in range(-radius, radius + 1):
-		var r1 = max(-radius, -q - radius)
-		var r2 = min(radius, -q + radius)
-		for r in range(r1, r2 + 1):
-			var hex = Vector3(q, r, -q-r)
-			_set_tile(hex, noise)
-
-
-func _set_tile(hex: Vector3, noise):
-	var noise_value = noise.get_noise_2d(hex.x, hex.y)
-	var tilemap_coords = hexmap.axial_to_oddq(hex)
-	
-	var tile = AdventureTile.new(
-		_gen_base_tile(hex, noise_value), 
-		_gen_details_tile(hex, noise_value),
-		hexmap.hex_length(hex) < 2
-		#true
-	)
-	
-	hexmap.add_hex(hex, tile)
-	baseTilemap.set_cell(tilemap_coords.x, tilemap_coords.y, tile.get_base_tile_id())
-	detailsTilemap.set_cell(tilemap_coords.x, tilemap_coords.y, tile.get_details_tile_id())
-
-
-func _gen_base_tile(hex: Vector3, value: float) -> int:
-	if value < -0.3:
-		return 1
-	elif value > 0.3:
-		return 2
-	else:
-		return 0
-
-func _gen_details_tile(hex: Vector3, value: float) -> int:
-	if value > 0 && value < 0.1:
-		return 0
-	return -1
-
-
-func _procedural_setup():
-	var rng = RandomNumberGenerator.new()
-	var simplexNoise = OpenSimplexNoise.new()
-	rng.randomize()
-	simplexNoise.seed = rng.randi()
-	simplexNoise.octaves = 3
-	simplexNoise.period = 16.0
-	simplexNoise.persistence = 0.5
-	simplexNoise.lacunarity = 3
-	return simplexNoise
+	$AdventureMapGenerator.gen_hexmap(hexmap, 16)
+	var all_hex = hexmap.get_all_hex()
+	for hex in all_hex:
+		var tile = all_hex[hex]
+		var tilemap_coords = hexmap.axial_to_oddq(hex)
+		 
+		baseTilemap.set_cell(tilemap_coords.x, tilemap_coords.y, tile.get_base_tile_id())
+		detailsTilemap.set_cell(tilemap_coords.x, tilemap_coords.y, tile.get_details_tile_id())
---
+++ hommlite/src/adventure/map/AdventureMapGenerator.gd
@@ -0,0 +1,54 @@
+extends Node
+class_name AdventureMapGenerator
+
+var rng = RandomNumberGenerator.new()
+var simplexNoise = OpenSimplexNoise.new()
+
+func _ready():
+	rng.randomize()
+	simplexNoise.seed = rng.randi()
+	simplexNoise.octaves = 3
+	simplexNoise.period = 16.0
+	simplexNoise.persistence = 0.5
+	simplexNoise.lacunarity = 3
+
+
+func gen_hexmap(hexmap: HexMap, radius: int):
+	for q in range(-radius, radius + 1):
+		var r1 = max(-radius, -q - radius)
+		var r2 = min(radius, -q + radius)
+		for r in range(r1, r2 + 1):
+			var hex = Vector3(q, r, -q-r)
+			hexmap.add_hex(hex, _gen_tile(hexmap, hex, radius))
+
+
+func _gen_tile(hexmap: HexMap, hex: Vector3, radius: int) -> AdventureTile:
+	var noise_value = simplexNoise.get_noise_2d(hex.x, hex.y)
+	var distance = hexmap.hex_length(hex)
+	
+	if distance == radius:
+		return AdventureTile.new(2, -1, false)
+	elif distance < 2:
+		return AdventureTile.new(0, -1, true)
+	else:
+		return AdventureTile.new(
+			_gen_base_tile(hex, noise_value), 
+			_gen_details_tile(hex, noise_value),
+			false
+		)
+
+
+func _gen_base_tile(hex: Vector3, value: float) -> int:
+	if value < -0.3:
+		return 1
+	elif value > 0.3:
+		return 2
+	else:
+		return 0
+
+
+func _gen_details_tile(hex: Vector3, value: float) -> int:
+	if value > 0 && value < 0.1:
+		return 0
+	return -1
+
